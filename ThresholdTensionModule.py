1e8996a82e9bd6fe8be60e72ed8fc9e17c7926e8
Felipe Cesar Cuellar da Silva <felipecesarcuellar12@gmail.com>
2023-12-14 01:05:53 -0300

    VTModule second derivative implementation

---
+++ ThresholdTensionModule.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+
+def requirements():
+    # Função que verifica se os requisitos para se obter Vt foram cumpridos -> Ainda não implementado
+    return True
+
+# Extrai Vt pelo método da segunda derivada
+
+def execute(dataframes: list[pd.DataFrame]):
+    print("\n ======= Módulo de extração de Vt ========")
+    if not requirements(): 
+        print("[ERRO] Dados insufientes para se obter Vt")
+        return
+    print(f"[INFO] Etapa atual: extrair Vt pela segunda derivada")
+    for df in dataframes:
+        print(f"[INFO] Arquivo: {df.attrs['Filename']} - Cascata: {df.attrs['Cascata']} - W: {df.attrs['W']} - L: {df.attrs['L']}")
+        df = df.sort_values(by='VG')
+
+        # Aplicar Savitzky-Golay para suavizar a curva original
+        # Script original em LabTalk: Window_size = 24 order = 3
+        window_size_smooth = 17
+        order_smooth = 3
+        window_size_derivative = 17
+        order_derivative = 3
+
+        df['Id_smoothed'] = savgol_filter(df['Id'], window_size_smooth, order_smooth)
+        df['Id_first_derivative'] = np.gradient(df['Id_smoothed'])
+        df['Id_smoothed_derivative'] = savgol_filter(df['Id_first_derivative'], window_size_derivative, order_derivative)
+        df['Id_second_derivative'] = np.gradient(df['Id_smoothed_derivative'])
+        second_derivative = df['Id_second_derivative']
+
+        max_index = np.argmax(second_derivative)
+        print(f"       Vt estimado: {df['VG'][max_index]} V")
+
+        fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(8, 6))
+
+        ax1.plot(df['VG'], df['Id_smoothed'], label='Curva Original Suavizada')
+        ax1.set_ylabel('Id')
+        ax1.set_title('Curva Original Suavizada')
+
+        ax2.plot(df['VG'], second_derivative, label='Segunda Derivada Suavizada')
+        ax2.scatter(df['VG'][max_index], second_derivative[max_index], color='red', label=f'Max: VG={df["VG"][max_index]}, 2ª Derivada={second_derivative[max_index]}')
+        ax2.set_xlabel('VG')
+        ax2.set_ylabel('Segunda Derivada Suavizada')
+        ax2.set_title('Segunda Derivada Suavizada da Curva')
+
+        ax1.legend()
+        ax2.legend()
+
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.9)
+        fig.suptitle(f"Cascata: {df.attrs['Cascata']} - W: {df.attrs['W']} - L: {df.attrs['L']} - Arquivo: {df.attrs['Filename']}")
+        plt.show()
